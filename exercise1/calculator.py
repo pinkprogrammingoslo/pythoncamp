d5baba866d00f8473542891589b096c706efefa5
Simone Andersson <simone@pinkprogramming.se>
2019-03-30 11:28:30 +0100

    Updated the calculator to be look "nicer"

--- exercise1/calculator.py
+++ exercise1/calculator.py
@@ -1,43 +1,48 @@
 def add(a, b):
     return a + b
 
+
 def sub(a, b):
     return a - b
 
+
 def mul(a, b):
     return a * b
+
 
 def div(a, b):
     return a / b
 
 
+def calculate(op, first_num, second_num):
+    if op == 1:
+        return add(first_num, second_num)
+    elif op == 2:
+        return sub(first_num, second_num)
+    elif op == 3:
+        return mul(first_num, second_num)
+    else:
+        return div(first_num, second_num)
+
+
+# The program
 print('Welcome to Python awesome calculator!')
 cont = 'y'
 while cont == 'y':
-    print('What type of calculation do you want to perform?')
-    print('Enter 1 for add (+)')
-    print('Enter 2 for subtract (-)')
-    print('Enter 3 for multiply (*)')
-    print('Enter 4 for divide (/)')
+    operation = int(input('What type of calculation do you want to perform? \n'
+                          'Enter 1 for add (+) \n'
+                          'Enter 2 for subtract (-) \n'
+                          'Enter 3 for multiply (*) \n'
+                          'Enter 4 for divide (/) \n'))
 
-    operation = int(input(''))
+    if operation not in range(1, 5):
+        print('That is not a valid operation, please try again')
+        continue
+
     first = int(input('Enter the first number: '))
     second = int(input('Enter the second number: '))
-    global result
 
-    if operation == 1:
-        result = add(first, second)
-    elif operation == 2:
-        result = sub(first, second)
-    elif operation == 3:
-        result = mul(first, second)
-    elif operation == 4:
-        result = div(first, second)
-    else:
-        print('That is not a valid operation, please try again')
-
-    print('The result is {}'.format(result))
-
+    print('The result is {}'.format(calculate(operation, first, second)))
     cont = input('Do you want to do another caluclation (y/n)? ')
 
 print('Thanks for using me! See you soon!')
